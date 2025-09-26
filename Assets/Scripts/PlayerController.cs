82b41a3dae3245b6c466b3e877ffe1d6d4df45be
Sayoregg <85542072+Sayoregg@users.noreply.github.com>
2025-09-26 21:59:56 +0200

    Turning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine.InputSystem.XR;
 
 public class PlayerController : MonoBehaviour
 {
+    //Public Variables
+    public GameObject footLeft;
+    public GameObject footRight;
+    public GameObject body;
+    public GameObject bodyPivot;
+
     //Private Variables
     private CharacterController controller;
 
@@ -13,6 +19,7 @@ public class PlayerController : MonoBehaviour
 
     //Movement variables
     public float moveSpeed = 10f;
+    private float walkingTimer = 0f;
 
 
     void Start()
@@ -35,13 +42,49 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //Movement
+        Walking();
+        Turning();
     }
     private void Walking()
     {
         //Movement
         Vector2 movementInput = moveAction.ReadValue<Vector2>();
         controller.Move(new Vector3(movementInput.x * moveSpeed * Time.deltaTime, 0));
+
+        //Animation
+        if (movementInput.magnitude > 0)
+        {
+            walkingTimer += Time.deltaTime;
+        }
+        else
+        {
+            walkingTimer = 0f;
+        }
+        footLeft.transform.position = new Vector3(transform.position.x + (1 * Mathf.Sin(2 * Mathf.PI * 1.5f * walkingTimer)), footLeft.transform.position.y, footLeft.transform.position.z);
+        footRight.transform.position = new Vector3(transform.position.x - (1 * Mathf.Sin(2 * Mathf.PI * 1.5f * walkingTimer)), footRight.transform.position.y, footRight.transform.position.z);
+    }
+
+    private void Turning()
+    {
+        Vector2 mousePos, guzzlerPos;
+        mousePos = Mouse.current.position.ReadValue();
+        guzzlerPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        Vector2 relativeVector = mousePos - guzzlerPos;
+        float guzzlerAngle = Mathf.Atan2(relativeVector.y, relativeVector.x) * (180 / Mathf.PI);
+
+        body.transform.rotation = Quaternion.Euler(-guzzlerAngle, 90, 0);
+
+        if (Mathf.Abs(guzzlerAngle) > 90)
+        {
+            bodyPivot.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        } else
+        {
+            bodyPivot.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        Debug.Log(guzzlerAngle);
     }
 
 }
