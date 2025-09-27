518f00ea1d6891fd4d2d9b9a7bd0fc20b778d833
Sayoregg <85542072+Sayoregg@users.noreply.github.com>
2025-09-27 21:26:17 +0200

    WinText

--- Assets/Scripts/SandTextManager.cs
+++ Assets/Scripts/SandTextManager.cs
@@ -8,17 +8,24 @@ public class SandTextManager : MonoBehaviour
     private TextMeshProUGUI text;
     private int sandNumber;
     public int sandCollected;
+    public GameObject winText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         text = GetComponent<TextMeshProUGUI>();
-        sandNumber = GameObject.FindGameObjectsWithTag("Sand").Count();
+        sandNumber = GameObject.FindGameObjectsWithTag("Sand").Count() -1;
         sandCollected = 0;
+        winText.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         text.text = $"{sandCollected}" + "/" + $"{sandNumber}";
+
+        if (sandNumber == sandCollected)
+        {
+            winText.SetActive(true);
+        }
     }
 }
